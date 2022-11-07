21e871d83f3e5f20ab320310d976725cf8da6b27
qzc_site <616042898@qq.com>
2022-11-07 16:33:41 +0800

    无改动

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 
-from core.utility import Path
+import sys
+from PyQt5.QtWidgets import QApplication
+from libGui.pyPyinstaller import PyPyinstaller
 
-print(Path())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    win = PyPyinstaller()
+    win.show()
+
+    sys.exit(app.exec_())
